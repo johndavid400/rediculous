d1befe0b6de20b35050093631cd03b9204e93945
JD_home <johndavid400@yahoo.com>
2011-09-16 01:34:20 -0500

    [FIRST COMMIT]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rediculous::Application.config.secret_token = '3f15fff712a16f7194aa8943d3306d8931f4cd4970382257fcf37226d040a2562f8dedd612c1c00a2cfa8eaf516f094cf8dc35d3df4462292bcc618da170af8d'
